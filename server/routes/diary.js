9c34c779736288b26c185c0b47419926d445278a
farhanScripts <farhan.dev06@gmail.com>
2024-06-07 10:32:08 +0700

    full CRUD diary feature

--- server/routes/diary.js
+++ server/routes/diary.js
@@ -4,5 +4,9 @@ const diaryController = require('../controllers/diaryControllers');
 const { isAuthenticated } = require('../middleware/isAuthenticated');
 
 router.get('/', isAuthenticated, diaryController.getAllDiary);
+router.get('/add', diaryController.addDiary);
+router.post('/add', diaryController.newDiary);
 router.get('/:id', diaryController.getDiaryDetail);
+router.put('/:id', diaryController.updateDiary);
+router.delete('/:id/delete', diaryController.deleteDiary);
 module.exports = router;
